bee3160d73142105a215725d90af86905a836c3f
naneunyamini <minp4028@gmail.com>
2026-08-14 15:31:26 +0900

    feat: 한입북스 UI 구현 및 SSR 소개 및 실습

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: {
+      books,
+    },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
