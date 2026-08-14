2d44f34e690db1d91c733740f490f6190a941a5b
naneunyamini <minp4028@gmail.com>
2026-08-14 15:30:15 +0900

    feat: 글로벌 레이아웃과 공통 스타일 구성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import GlobalLayout from "@/components/global-layout";
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: NextPageWithLayout }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
